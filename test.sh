6aa4a290688635683e37881484e8425f3f3e7fd3
mbeuil <mbeuil@student.42.fr>
2020-10-01 01:59:19 +0200

    type error

--- test.sh
+++ test.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# ----------------------
+# ---------------
 # Color Variables
-# ----------------------
+# ---------------
+
 RED="\033[0;31m"
 YELLOW='\033[1;33m'
 GREEN='\033[1;32m'
 LCYAN='\033[1;36m'
 NC='\033[0m' # No Color
+
+# ------------
+# Progress Bar
+# ------------
+
+BAR='####################'   # this is full bar, e.g. 20 chars
 
 # --------------
 # Pause function
@@ -35,6 +42,10 @@ echo -e "/*                                                                     
 echo -e "/* ************************************************************************** */${LCYAN}"${NC}
 echo
 
+echo
+echo -e "${GREEN}Starting a new React APP ... ${NC}"
+echo
+
 # ---------------------
 # REACT APP CONFIG TOOL 
 # ---------------------
@@ -46,20 +57,22 @@ echo
 
 # Framework Manager prompt
 
-echo "Starting a new React APP"
+echo "Which framework are you using ?"
 select framework_choices in "create-react-app" "Cancel"; do
   case $framework_choices in
     create-react-app ) framework_init="npx create-react-app ${app_name}"; break;;
     Cancel ) exit;;
   esac
 done
-echo
 
+echo
 echo -e "${LCYAN}Creating your repo and installing your ${app_name} react app... ${NC}"
-$framework_init
 
 echo
-echo "Switch to ${app_name} directory"
+$framework_init 
+
+echo
+echo "Switch to ${app_name} directory."
 cd ${app_name}
 
 # --------------------------------------
@@ -69,7 +82,7 @@ cd ${app_name}
 # Package Manager Prompt
 
 echo
-echo "Which package manager are you using?"
+echo "Which package manager are you using ?"
 select package_command_choices in "Yarn" "npm" "Cancel"; do
   case $package_command_choices in
     Yarn ) pkg_cmd='yarn add'; break;;
@@ -81,7 +94,7 @@ echo
 
 # File Format Prompt
 
-echo "Which ESLint and Prettier configuration format do you prefer?"
+echo "Which ESLint and Prettier configuration format do you prefer ?"
 select config_extension in ".js" ".json" "Cancel"; do
   case $config_extension in
     .js ) config_opening='module.exports = {'; break;;
@@ -150,9 +163,10 @@ if [ "$skip_eslint_setup" == "true" ]; then
   break
 else
   echo
-  echo -e "4/6 ${YELLOW}Building your .eslintrc${config_extension} file...${NC}"
+  echo -e "4/6 ${YELLOW}Config file for Eslintrc builded !${NC}"
   > ".eslintrc${config_extension}" # truncates existing file (or creates empty)
 
+  echo
   echo ${config_opening}'
   "extends": [
     "airbnb",
@@ -181,7 +195,7 @@ fi
 if [ "$skip_prettier_setup" == "true" ]; then
   break
 else
-  echo -e "5/6 ${YELLOW}Building your .prettierrc${config_extension} file... ${NC}"
+  echo -e "5/6 ${YELLOW}Config file for Prettier builded !${NC}"
   > .prettierrc${config_extension} # truncates existing file (or creates empty)
 
   echo ${config_opening}'
@@ -207,7 +221,7 @@ fi
 # Vscode config
 
 echo
-echo "Are you using vscode?"
+echo "Are you using vscode ?"
 select editor_choices in "yes" "no" "Cancel"; do
   case $editor_choices in
     yes ) skip_vscode='false'; break;;
@@ -220,7 +234,7 @@ echo
 if [ "$skip_vscode" == "true" ]; then
   break
 else
-  echo -e "6/6 ${YELLOW}Building your .vscode config file... ${NC}"
+  echo -e "6/6 ${YELLOW}Config file for .vscode builded !${NC}"
   > .prettierrc${config_extension} # truncates existing file (or creates empty)
 
   mkdir .vscode
@@ -299,11 +313,15 @@ fi
 # Check for conflict
 # ------------------
 
+echo
+echo -e "${GREEN}Configuring other libraries... ${NC}"
 
 # CSS in JS libraries
 
+css_cmd=false
+
 echo
-echo "Which CSS in JS library will you use?"
+echo "Which CSS in JS library will you use ?"
 select css_library_choices in "emotion" "styled-components" "Cancel"; do
   case $css_library_choices in
     emotion ) css_cmd="$pkg_cmd emotion"; break;;
@@ -312,13 +330,17 @@ select css_library_choices in "emotion" "styled-components" "Cancel"; do
   esac
 done
 
-echo
-echo -e "${YELLOW}Installing yout CSS in JS library...${NC}"
+if [ "$css_cmd" == "false" ]; then
+  break
+else
+  echo
+  echo -e "${YELLOW}Installing yout CSS in JS library...${NC}"
+  echo
+  $css_cmd
+  echo
+fi
+
 
 echo
-$css_cmd
-echo
-
-echo
-echo -e "${GREEN}Finished setting up!${NC}"
+echo -e "${GREEN}Finished setting up !${NC}"
 echo
